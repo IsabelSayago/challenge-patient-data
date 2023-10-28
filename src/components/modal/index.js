53abf7ecf4c3ec0d0ec167640e6f27f8a475d08d
Isabel <isabelfsayagob@gmail.com>
2023-10-28 14:50:19 -0300

    add: textfields values in modal

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,47 +1,102 @@
 import React from 'react';
-import { useContext } from 'react';
-import Box from '@mui/material/Box';
+import { useContext, useState } from 'react';
+import './index.css';
+
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import Create from '@mui/icons-material/Create'
+import CloseIcon from '@mui/icons-material/Close';
 import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+
+import TextField from '@mui/material/TextField';
+
 
 import { PatientsContext } from "../../reducer";
 import { setModalData } from "../../actions";
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  borderRadius: '5px',
-  boxShadow: 24,
-  p: 4,
-};
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
 
 const CustomModal = () => {
   const { dispatch, state } = useContext(PatientsContext);
   const { modalData } = state;
+  const [textFields, setTextFields] = useState(Object.keys(modalData).map(value => ({
+    disabled: true,
+    value: modalData[value],
+  })))
 
-  const handleClose = () => setModalData({ dispatch, payload: false});
+  const handleClose = () => setModalData({ dispatch, payload: false });
+
+  const onClickIconEdit = (index) => {
+    const newTextFields = [...textFields];
+    newTextFields[index].disabled = !newTextFields[index].disabled;
+    console.log('newTextFields[index].disabled', newTextFields[index].disabled);
+    setTextFields(newTextFields);
+  };
 
   return (
     <div>
-      <Modal
-        open={modalData}
+      <BootstrapDialog
+        className='modal'
         onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        aria-labelledby="customized-dialog-title"
+        open={modalData}
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            {modalData.name}
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+          {modalData.name}
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <DialogContent dividers>
+        <Typography gutterBottom>
             {modalData.website}
           </Typography>
-        </Box>
-      </Modal>
+          {textFields.map((item, index) => (
+            <div className='container-field' key={item.value}>
+              <TextField
+                disabled={item.disabled}
+                className='textfield'
+                id="mail"
+                label="Required"
+                defaultValue={`${item.value.toLowerCase().replace(' ', '')}@gmail.com`}
+              />
+              <button className='icon-edit' onClick={() => onClickIconEdit(index)}>
+                <Create />
+              </button>
+
+            </div>
+          ))}
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Save changes
+          </Button>
+          <Button autoFocus onClick={handleClose}>
+            Cancel
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
     </div>
   );
 };
