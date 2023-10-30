7696be0659dd0a72b23e9ec16b01f31e0bc08feb
Isabel <isabelfsayagob@gmail.com>
2023-10-30 11:39:05 -0300

    adjust: service return

--- src/services/index.js
+++ src/services/index.js
@@ -1,20 +1,47 @@
-const API_PATIENTS = 'https://63bedcf7f5cfc0949b634fc8.mockapi.io/users';
+const API_GET_PATIENTS = 'https://63bedcf7f5cfc0949b634fc8.mockapi.io/users';
+const API_CREATE_PATIENT = '';
 
 export class PatientsService {
 
     static getAllPatients = async () => {
+        let resp = null;
         try {
-            const response = await fetch(API_PATIENTS, {
+            const response = await fetch(API_GET_PATIENTS, {
                 method: 'GET'
             })
 
-            console.log('response', response);
-            if (!response) {
-                throw new Error('Error while retrieving');
+            if (!response.ok) {
+                throw new Error('Error while retrieving patients');
             }
-            return response.json();
+
+            resp = await response.json();
+
+            return resp;
         } catch (error) {
-            return null;
+            console.error(error);
+            
+            return resp;
+        }
+
+    };
+
+    static addNewPatient = async ({ data }) => {
+        let resp = null;
+        try {
+            const response = await fetch(API_CREATE_PATIENT, {
+                method: 'POST',
+                body: data,
+            });
+
+            if (!response) {
+                throw new Error('Error while creating the patient');
+            }
+
+            resp = await response.json();
+
+            return resp;
+        } catch (error) {
+            return resp;
         }
 
     };
